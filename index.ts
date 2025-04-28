55ab6693580bd13d4f56ff2db02419a8a8be5e04
GouravSingh2005 <gouravsingh56466@gmail.com>
2025-04-28 22:23:10 +0530

    new changes

--- index.ts
+++ index.ts
@@ -1,5 +1,27 @@
 import express from "express";
-import { middleware } from "./middleware";
+
+import type { Request,Response,NextFunction } from "express";
+import client from "prom-client";
+
+const requestCounter=new client.Counter({
+    name: 'http_request_total',
+    help : 'Total number of HTTP requests',
+    labelNames: ['method', 'route', 'status_code']
+});
+function middleware(req:Request,res: Response,next:NextFunction){
+    const startTime=Date.now();
+
+    res.on('finish',()=>{
+        const endTime=Date.now();
+        console.log(`Request Took ${endTime-startTime}ms`)
+        requestCounter.inc({
+            method:req.method,
+            route:req.route? req.route.path:req.path,
+            status_code:res.statusCode
+        });
+    });
+    next();
+};
 const app=express();
 app.use(middleware);
 
@@ -17,6 +39,11 @@ app.get("/users",(req,res)=>{
     res.json({
         message:"user"
     })
+})
+app.get("/metrics", async (req, res) => {
+    const metrics = await client.register.metrics();
+    res.set('Content-Type', client.register.contentType);
+    res.end(metrics);
 })
 
 app.listen(3000);
